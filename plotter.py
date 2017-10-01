86c0597c3635fdf99536a608bc918e7730be23eb
Tim Berry <mit111@gmail.com>
2017-10-01 16:10:21 +0100

    plotter.py changes

--- plotter.py
+++ plotter.py
@@ -1,55 +1,44 @@
-from mpl_toolkits.basemap import Basemap
 import sqlite3
 import os
+import numpy as np
+from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+from matplotlib.collections import LineCollection
 
+# Database setup
 dir_path = os.path.dirname(os.path.abspath(__file__))
 db = sqlite3.connect(os.path.join(dir_path, 'data.db'))
 cursor = db.cursor()
 
-'''
-Sometimes the aircraft data provided by Whazzup is empty.
-If the data is empty, it throws an error in the program.
-This should clear any empty cells.
-'''
+# Map setup
+map = Basemap(projection='cyl',resolution='c')
+map.drawmapboundary(fill_color='#424242')
+map.fillcontinents(color='#636363')
+
+# Clear any empty data which will cause the program to crash
 cursor.execute('DELETE FROM plots WHERE lat IS NULL OR lat = \'\';')
 db.commit()
 
 cursor.execute('SELECT DISTINCT vid FROM plots;')
 vids = [x[0] for x in cursor.fetchall()]
 
-lat = []
-lon = []
-
-map = Basemap(projection='cyl',resolution='c')
-
-map.drawmapboundary(fill_color='#424242')
-map.fillcontinents(color='#636363')
-
 for vid in vids:
-    cursor.execute('SELECT lat, lon FROM plots WHERE vid = ?;', (vid,))
-    plots = cursor.fetchall()
-    for plot in plots:
-        try:
-            cross_meridian = abs(plot[1]) + abs(lon[-1])
-            delta_lon = abs(abs(plot[1]) - abs(lon[-1]))
-            delta_lat = abs(abs(plot[0]) - abs(lat[-1]))
-        except IndexError:
-            cross_meridian = False
-            delta_lon = False
-            delta_lat = False
+    cursor.execute('SELECT lat, lon, alt FROM plots WHERE vid = ?;', (vid,))
 
-        if (cross_meridian) > 358 or (delta_lon > 0.5) or (delta_lat > 0.5):
-            map.plot(lon, lat, marker=None,color='deepskyblue',linewidth=0.2)
-            lat = []
-            lon = []
+    lat, lon, alt = zip(*cursor.fetchall())
+    alt = np.array(alt)
 
-        lat.append(plot[0])
-        lon.append(plot[1])
-    map.plot(lon, lat, marker=None,color='deepskyblue',linewidth=0.2)
-    lat = []
-    lon = []
+    if (np.all(np.diff(lat) < 0.5)) and (np.all(np.diff(lat) < 0.5)):
+        points = np.array([lon, lat]).T.reshape(-1, 1, 2)
+        segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+        lc = LineCollection(segments, cmap=plt.get_cmap('ocean'),
+                            norm=plt.Normalize(0, 60000))
+        lc.set_array(alt)
+        lc.set_linewidth(1)
+
+        plt.gca().add_collection(lc)
 
 plt.show()
-
 db.close()
